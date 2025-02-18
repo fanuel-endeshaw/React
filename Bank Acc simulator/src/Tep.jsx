9d38aff9e65fec2c5c59e2a5aa8d0c469b0f9cb8
Fanuel Endeshaw <fanuelite@gmail.com>
2025-02-18 01:05:02 -0800

    bankAccountSimulator v1

---
+++ Bank Acc simulator/src/Tep.jsx
@@ -0,0 +1,89 @@
+import  { useReducer } from "react";
+function reducer(state, action) {
+  if (action.type === "enable") {
+    return { balance: 500, loan: 0, isDisabled: false };
+  } else if (action.type === "deposit") {
+    return {
+      balance: state.balance + action.payload,
+      loan: 0,
+      isDisabled: false,
+    };
+  } else if (action.type === "withdraw") {
+    return {
+      balance: state.balance - action.payload,
+      loan: 0,
+      isDisabled: false,
+    };
+  } else if (action.type === "loan") {
+    return {
+      balance: state.balance + action.payload,
+      loan: action.payload,
+      isDisabled: false,
+    };
+  } else if (action.type === "ploan") {
+    return {
+      balance: state.balance - action.payload,
+      loan: action.payload,
+      isDisabled: false,
+    };
+  } else if (action.type === "close") {
+    return {
+      ...initial,
+    };
+  }
+}
+const initial = { isDisabled: true, balance: "", loan: "" };
+const Tep = () => {
+  const [count, dispatch] = useReducer(reducer, initial);
+  const style = { margin: "5px 0" };
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        width: "100vw",
+        height: "100vh",
+        flexDirection: "column",
+        gap: "1rem",
+      }}
+    >
+      <h1 style={{ marginBottom: "0" }}>useReducer Bank account</h1>
+      <p style={style}>Balance:{count.balance}</p>
+      <p style={style}>Loan:{count.loan}</p>
+
+      <button onClick={() => dispatch({ type: "enable" })}>Open account</button>
+      <button
+        disabled={count.isDisabled}
+        onClick={() => dispatch({ payload: 150, type: "deposit" })}
+      >
+        deposit 150
+      </button>
+      <button
+        disabled={count.isDisabled}
+        onClick={() => dispatch({ payload: 50, type: "withdraw" })}
+      >
+        withdraw 50
+      </button>
+      <button
+        disabled={count.isDisabled}
+        onClick={() => dispatch({ payload: 5000, type: "loan" })}
+      >
+        request a loan of 5000
+      </button>
+      <button
+        disabled={count.isDisabled}
+        onClick={() => dispatch({ payload: 5000, type: "ploan" })}
+      >
+        pay a loan
+      </button>
+      <button
+        disabled={count.isDisabled}
+        onClick={() => dispatch({ type: "close" })}
+      >
+        close account
+      </button>
+    </div>
+  );
+};
+
+export default Tep;
